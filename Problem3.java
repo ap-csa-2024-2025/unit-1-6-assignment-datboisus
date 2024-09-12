5aee148e189f6302a549a950e1e9e8ce3f791d01
datboisus <donvu251106@gmail.com>
2024-09-12 10:16:24 -0700

    3

--- Problem3.java
+++ Problem3.java
@@ -1,1 +1,9 @@
-
+public class Problem3
+{
+    public static void main(String[] args)
+    {
+        double num = 69.123;
+        int numup = (int) (num * 1000);
+        System.out.println(numup);
+    }
+}
