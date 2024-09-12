41aeb5af46beba5d44e4998c1152aca76ea548a6
datboisus <donvu251106@gmail.com>
2024-09-12 10:09:00 -0700

    1-2

--- Problem2.java
+++ Problem2.java
@@ -1,1 +1,20 @@
+public class Problem2
+{
+    public static void main(String[] args)
+    {
+        double num1 = 54.1323815896;
+        double num2 = 349.345265146443;
+        System.out.println("Two Original digits:");
+        System.out.println(num1);
+        System.out.println(num2);
 
+        int num1rounded = (int) num1;
+        int num2rounded = (int) num2;
+        System.out.println(num1rounded);
+        System.out.println(num2rounded);
+
+
+
+        
+    }
+}
